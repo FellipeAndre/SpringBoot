dab44c5d8f631d463dfcedeee2a272d88b2b1aee
FellipeAndre <felipetobiasandre.jc@gmail.com>
2023-01-09 20:38:07 -0300

    Ajustando ambiente

---
+++ config/src/main/java/br/com/test/TestApplication.java
@@ -0,0 +1,25 @@
+package br.com.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TestApplication {
+
+    @Value("${application.name}")
+    private String applicationName;
+
+   @GetMapping("/hello")
+    public String helloWord(){
+        return applicationName ;
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
